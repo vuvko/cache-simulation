0e8d4a6e0b0eaad5fbdac7305eeee82aa1280674
vuvko <notvuvko@gmail.com>
2011-11-28 19:23:54 +0300

    Adding cache.c, cache.h and cleaning repo.

---
+++ cache.c
@@ -0,0 +1,21 @@
+#include "cache.h"
+#include "direct_cache.h"
+#include "full_cache.h"
+
+#include <string.h>
+
+AbstractMemory *
+cache_create(ConfigFile *cfg, const char *var_prefix, StatisticsInfo *info, AbstractMemory *mem, Random *rnd)
+{
+    const char *a = config_get(cfg, "associativity");
+    if (!a) {
+        error_undefined("cache_create", "associativity");
+    } else if (!strcmp(a, "full")) {
+        return full_cache_create(cfg, var_prefix, info, mem, rnd);
+    } else if (!strcmp(a, "direct")) {
+        return direct_cache_create(cfg, var_prefix, info, mem, rnd);
+    } else {
+        error_invalid("cache_create", "associativity");
+    }
+    return NULL;
+}
